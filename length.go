c877f512dcaee90744afbc4421e961a7ef548c26
dustinevan <dustinevancurrie@gmail.com>
2017-10-16 21:18:49 -0600

    add mult to chron.Length interface

--- length.go
+++ length.go
@@ -1,8 +1,12 @@
 package chron
 
-import "time"
+import (
+	"time"
+	"github.com/dustinevan/chron/length"
+)
 
 type Length interface {
+	Mult(int) length.Duration
 	Years() int
 	Months() int
 	Days() int
