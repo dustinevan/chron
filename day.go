016b23f7e6a3122659505c17a1869bfe78ec8d50
dustinevan <dustinevancurrie@gmail.com>
2018-01-01 14:06:28 -0700

    fix imports and change NowDay() to Today()

--- day.go
+++ day.go
@@ -15,7 +15,7 @@ func NewDay(year int, month time.Month, day int) Day {
 	return Day{time.Date(year, month, day, 0, 0, 0, 0, time.UTC)}
 }
 
-func NowDay() Day {
+func Today() Day {
 	return Now().AsDay()
 }
 
