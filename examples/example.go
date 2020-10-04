ab310e1238be0b224ef740077bd5a9f37761b21a
Dustin Currie <dustinevancurrie@gmail.com>
2020-10-04 16:42:58 -0600

    Revert "remove work in progress stuff"
    This reverts commit 68e6d4af83811d616be30174f7865944b1c91368.

---
+++ examples/example.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/dustinevan/chron/seq"
+)
+
+func main() {
+	seq.ExampleDateRange()
+}
---
+++ seq/basics_test.go
@@ -0,0 +1,23 @@
+package seq
+
+import (
+	"time"
+	"fmt"
+	"github.com/dustinevan/chron"
+)
+
+func ExampleDateRange() {
+	daterange, stop := DateRange(chron.NewDay(2017, time.December, 31), chron.NewDay(2018, time.February, 1) )
+	i := 0
+	for t := range daterange {
+		if i == 2 {
+			stop()
+		}
+		fmt.Println(t)
+		i++
+	}
+	// Output:
+	// 2017-12-31 00:00:00 +0000 UTC
+	// 2018-01-01 00:00:00 +0000 UTC
+	// 2018-01-02 00:00:00 +0000 UTC
+}
